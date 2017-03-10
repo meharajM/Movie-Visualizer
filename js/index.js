fceab2f97264a13f1350862645ec6524fe269388
meharaj <meharaj.m@snapwiz.com>
2017-03-10 16:02:53 +0530

    Implementation of barchart

--- js/index.js
+++ js/index.js
@@ -29,6 +29,25 @@ search.addEventListener('click', function(){
      pie.dimension(catDimension);
      pie.group(catGroup);
      pie.render();
+
+     //Bar Chart 
+     var bar = new dc.barChart('#bar-chart');
+     var nameDimention = data.dimension(function(d){return d.show_title});
+     debugger
+     var ratingGroup = nameDimention.group().reduceSum(function(d){
+      return d.rating;
+     });    
+     bar.dimension(nameDimention);
+     bar.group(ratingGroup);
+     bar.gap(3);
+  
+     bar.x(d3.scale.ordinal());//use ordinal scale whenever u are not dealing with the set of real numbers or series of numbers which are not so random. https://github.com/d3/d3-3.x-api-reference/blob/master/Ordinal-Scales.md
+     bar.xUnits(dc.units.ordinal); //to tell dc that using ordinal scale    
+     bar.width(500);
+     bar.height(500);
+     
+     bar.render();
     });
   });  
 });
+ 
